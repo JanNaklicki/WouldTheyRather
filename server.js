661c6a4b1149719dbfaf8617b72128dcbe05c5bd
JanNaklicki <49452909+JanNaklicki@users.noreply.github.com>
2021-01-18 01:26:06 +0100

    Changes to be committed: 	modified:   package.jso new file:   public/javascripts/socket.io.js

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 // load the things we need
 
 const SerialPort = require('serialport')
-var express = require('express');
-var request = require('request')
 
-const socketio = require('socket.io')
-var http = require('http')
-var app = express();
+const express = require('express');
+
+const app = express()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+
+
+
+
+app.use(express.static('public'))
+
+
+
+
+
 const Readline = SerialPort.parsers.Readline
 
-
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+  })
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -20,7 +34,7 @@ const serialport = new SerialPort('COM3')
 
 
 // use res.render to load up an ejs view file
-app.use(express.static('public'))
+
 
 
 
@@ -43,34 +57,37 @@ app.post('/serial',function(req,res){
     const parser = new Readline();
     serialport.pipe(parser);
     parser.on('data',function(data){
-
-        console.log(data)
         
+        io.emit('serialD',data);
 
+        console.log(data);
 
-
-       
     });
     console.log('works!');
     
-    res.status(200).end();
+    
 })
-// about page
+
+
+
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log("server is running")
-})
-
- const io = socketio(server)
-
 
 io.on('connection', (socket) => {
-    console.log("New user connected")
-})
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    
+});
 
 
 
+
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
 
