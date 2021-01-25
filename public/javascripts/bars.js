47805baf1685ffad123d8ab74f532dc7fed4af63
JanNaklicki <49452909+JanNaklicki@users.noreply.github.com>
2021-01-25 03:46:52 +0100

    First two bars connected to serial data

--- public/javascripts/bars.js
+++ public/javascripts/bars.js
@@ -1,14 +1,77 @@
-const bars = document.querySelectorAll('.bar');
-const progress = document.querySelectorAll('.progress');
+// const randomWidth = Math.floor((Math.random() * 65) + 10);
+// let a = `${100-randomWidth}%`;
+// console.log(randomWidth);
+// console.log(a);
 
-bars.forEach((bar, index) => {
-  const randomWidth = Math.floor((Math.random() * 65) + 10);
-  bar.style.height = `${randomWidth}%`;
+// bars[0].style.height = `${randomWidth}%`;
+// bars[1].style.height = a;
 
-  progress[index].addEventListener('mouseover', () => {
-    const randomTiming = Math.floor((Math.random() * 2) + 2);
-    console.log(randomTiming);
-    bar.style.transitionDuration = `${randomTiming}s`;
-    bar.style.height = '100%';
-  });
-})
+// progress[0].addEventListener('mouseover', () => {
+//     const randomTiming = Math.floor((Math.random() * 2) + 2);
+
+//     bars[0].style.transitionDuration = `${randomTiming}s`;
+//     bars[1].style.transitionDuration = `${randomTiming}s`;
+//     bars[0].style.height = '100%';
+//     bars[1].style.height = '100%';
+
+// });
+
+
+
+
+
+
+
+
+
+// bars.forEach((bar, index) => {
+//     const randomWidth = Math.floor((Math.random() * 65) + 10);
+//     bar.style.height = `${randomWidth}%`;
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mapValue(val, in_min, in_max, out_min, out_max) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function float2int(value) {
+    return value | 0;
+}
+
+var socket = io();
+
+var serData = document.getElementById('serD');
+var playerOne = document.getElementById('p1Pick')
+var playerTwo = document.getElementById('p2Pick')
+
+
+socket.on('serialD', function(msg) {
+    console.log("dupa");
+    var a = String(msg);
+    var c = a.split(':');
+    var one = c[0];
+    var two = c[1];
+    oneMapped = mapValue(one, 0, 1023, 0, 100);
+    twoMapped = mapValue(two, 0, 1023, 0, 100);
+    oneMapped = float2int(oneMapped)
+    twoMapped = float2int(twoMapped)
+
+    // playerTwo.textContent = oneMapped;
+    // playerOne.textContent = twoMapped;
+    serData.textContent = msg;
+    console.log(msg);
+
+});
