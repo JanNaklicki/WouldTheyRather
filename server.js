f14df19c2da38370cb0d7f259b5430773f99e9e8
JanNaklicki <49452909+JanNaklicki@users.noreply.github.com>
2021-02-02 23:07:29 +0100

    mall css edits

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const SerialPort = require('serialport')
 const express = require('express');
+require('./loaders/mongoose')
 
 
 
@@ -14,33 +15,35 @@ const parser = new Readline();
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
+
+
 serialport.pipe(parser);
 
+
+
+parser.on('data', function(data) {
+    io.emit('serialD', data);
+    console.log(data);
+});
+
+app.get('/', function(req, res) {
+
+    res.render('index')
+})
+
+app.get('/duel', function(req, res) {
+    res.render('duel', {});
+});
+
+
+
+// app.post('/serial', async(req, res) => {})
 
 // app.get('/test', function(req, res) {
 //     res.render('test', {
 
 //     });
 // })
-
-parser.on('data', function(data) {
-
-    io.emit('serialD', data);
-    console.log(data);
-});
-console.log('works!');
-
-
-
-app.get('/', function(req, res) {
-    res.render('index', {});
-});
-
-
-
-
-// app.post('/serial', async(req, res) => {})
-
 
 
 
